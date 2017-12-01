54e3014cdf42cd0ae6559c6a5d7db2378be40f85
Aaron Ross <ashmortar@gmail.com>
2017-12-01 10:54:41 -0800

    add hidden divs with the tracks that will be presented at end of survey and simplify javascript click function so that it is consistent across all non-submit buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
-$(document).ready(function() {
 
+
+
+
+
+
+
+
+$(document).ready(function() {
+//click function that hides current either start survey button or current questoin and displays next div object
   $("button.next").click(function(event) {
     event.preventDefault();
     $(this).parent().fadeOut();
     $(this).parent().next().fadeIn();
   })
+
+
 });
