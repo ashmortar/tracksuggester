34ff1e4e1726587f7c0a6f6917c7cda177611f52
Aaron Ross <ashmortar@gmail.com>
2017-12-01 11:39:54 -0800

    add content to form questions for beginnings of decision tree and add span into track suggestions to add user's name into text

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,8 @@
-
-
-
-
-
-
-
-
 $(document).ready(function() {
 //click function that hides current either start survey button or current questoin and displays next div object
   $("button.next").click(function(event) {
     event.preventDefault();
-    $(this).parent().fadeOut();
+    $(this).parent().hide();
     $(this).parent().next().fadeIn();
   })
 
