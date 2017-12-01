78f9716de6f0a3b25b3bd91fdd6fc6f132094e68
Aaron Ross <ashmortar@gmail.com>
2017-12-01 11:49:47 -0800

    add variable declarations in javascript and assigns values to the variables form the form answers

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,20 @@ $(document).ready(function() {
     $(this).parent().next().fadeIn();
   })
 
+//click function for submit button at end of Survey
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var queston1 = $("#question1").val();
+    var queston2 = $("#question2").val();
+    var queston3 = $("#question3").val();
+    var queston4 = $("#question4").val();
+    var queston5 = $("#question5").val();
+    var queston6 = $("#question6").val();
+    var queston7 = $("#question7").val();
+
+  //this will check to see if any questions were left blank and prompt the user to restart the quiz.
+  if ()
+  })
 
 });
